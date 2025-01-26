809a53891238d0333e8cdd5a8425eb3642f77fe5
ToumiFourat <fourattoumi021@gmail.com>
2025-01-26 12:43:52 +0100

    CP

---
+++ codeforces/J. Divisibility.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    long long ppcm = 2520;
+    cout << n / ppcm;
+    return 0;
+}
